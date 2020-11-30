acda745f91c9ffe5de829e9e3f1921968c9e10b2
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-30 20:59:37 -0300

    finalizando mais uma aula

--- src/componentes/cadastro/cadastro-clientes.js
+++ src/componentes/cadastro/cadastro-clientes.js
@@ -1,19 +1,24 @@
-const formCadastroCliente = document.querySelector("[data-form]")
+import validaCPF from '../validacao/validaCPF'
+import { cadastrarClientes } from '../../api/cliente'
 
-formCadastroCliente.addEventListener("submit",
-    event => { 
-        event.preventDefault()
+const eventoEnvia = (form) => {
 
-        const nome = event.target.querySelector("[data-nome]").value
-        const cpf = event.target.querySelector("[data-cpf]").value
+    const formCadastroCliente = document.querySelector("[data-form]")
 
-            if(validaCPF(cpf)){
+    formCadastroCliente.addEventListener("submit",
+        event => {
+            event.preventDefault()
+
+            const nome = event.target.querySelector("[data-nome]").value
+            const cpf = event.target.querySelector("[data-cpf]").value
+
+            if (validaCPF(cpf)) {
                 cadastrarClientes(nome, cpf)
-            } else { 
+            } else {
                 alert('O CPF não é válido')
             }
-           
-        
-        
-    }
-)
+        }
+    )
+}
+
+export default eventoEnvia
--- src/componentes/listagem/listagem-cliente.js
+++ src/componentes/listagem/listagem-cliente.js
@@ -1,17 +1,37 @@
-import { deletaCliente, listarClientes} from '../../api/cliente.js'
+import { deletaCliente, listarClientes } from '../../api/cliente.js'
 import "../../assets/css/clientes.css"
+import inicializaCadastro from '../cadastro/componente-cadastro'
 
 const removeCliente = (id) => {
-  if(confirm("Deseja deletar o cliente ?")){
-    deletaCliente(id)
-    window.location.reload()
-  }
+    if (confirm("Deseja deletar o cliente ?")) {
+        deletaCliente(id)
+        window.location.reload()
+    }
 }
 
-const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+const conteudo = `
+<thead class="thead-dark">
+      <tr>
+            <th scope="col">CPF</th>
+            <th scope="col">Nome</th>
+            <th scope="col"></th>
+            <th scope="col"> <a class="btn btn-primary" href="">Novo Cliente</a></th>
+            </th>
+      </tr>
+</thead>
+
+
+`
+const container = document.querySelector("[data-container]")
+const tabela = document.createElement('table')
+tabela.classList.add('table')
+tabela.innerHTML = conteudo
+container.appendChild(tabela)
+
+const corpoTabela = document.createElement('tbody')
 
 const exibeCliente = (cpf, nome, id) => {
-    const linha = document.createElement('tr');
+    const linha = document.createElement('tr')
 
     const conteudoLinha = `
     <td>${cpf}</td>
@@ -20,22 +40,19 @@ const exibeCliente = (cpf, nome, id) => {
     <a href="./componentes/edita/edita-clientes.html?id=${id}">
     <button type=""button class="btn btn-info">Editar</button>
     </a>
-    
-    
+     
 `
-  
+
     linha.innerHTML = conteudoLinha;
     return linha;
-  };
-  
-  listarClientes().then( exibe => {
-  exibe.forEach(indice => {
-    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
-  })
- }
+}
 
- )
-  
-  
- 
+listarClientes().then(exibe => {
+        exibe.forEach(indice => {
+            corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
+        })
+    }
 
+)
+
+tabela.appendChild(corpoTabela)
